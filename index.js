1ea8ddd14fafa9bb8d0d36fe04597a7a6fd7464e
Younes Bessa <bessa460@gmail.com>
2023-07-13 02:28:20 +0200

    🎨 update origin url in index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 
 dotenv.config();
 app.use(cors({
-    origin: 'https://client-bice-beta.vercel.app',
+    origin: 'https://client-one-topaz.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
